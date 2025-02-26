ade0adf3f65368001e0c9c97bcfd6b9f3688887d
Sair Marquez <sair2003.hotmail.com@gmail.com>
2025-02-26 12:54:22 -0500

    feat: repository mejorado

--- gen/repository.py
+++ gen/repository.py
@@ -1,13 +1,17 @@
 def generar_repository(nombre_entidad, paquete, id_tipo="Long"):
     """
-    Genera el código de la interfaz Repository, p. ej. PatientRepository.
+    Genera el código de la interfaz Repository, p. ej. PatientRepository,
+    extendiendo CrudRepository y JpaSpecificationExecutor, e incluyendo la anotación @Repository.
     """
     nombre_simple = nombre_entidad.replace("Entity", "")
     return f"""package {paquete}.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import {paquete}.models.entities.{nombre_entidad};
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface {nombre_simple}Repository extends JpaRepository<{nombre_entidad}, {id_tipo}> {{
+@Repository
+public interface {nombre_simple}Repository extends CrudRepository<{nombre_entidad}, {id_tipo}>, JpaSpecificationExecutor<{nombre_entidad}> {{
 }}
 """
